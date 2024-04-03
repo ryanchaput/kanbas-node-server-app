992e21ca8932219bbf803714e30223cc09bf1da5
ryanchaput <99510232+ryanchaput@users.noreply.github.com>
2024-04-03 00:20:11 -0400

    Fixing incorrect filepaths for import in Database/index.js

--- Kanbas/Database/index.js
+++ Kanbas/Database/index.js
@@ -1,6 +1,7 @@
-import courses from './courses';
-import students from './students';
-import modules from './modules';
-import users from './users';
-import assignments from './assignments';
-import grades from './grades';
+import enrollments from './enrollments.js';
+import modules from './modules.js';
+import users from './users.js';
+import assignments from './assignments.js';
+import grades from './grades.js';
+import courses from './courses.js';
+export default { courses, modules, assignments, users, grades, enrollments };

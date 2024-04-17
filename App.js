5769c53c862b8ec8da68a8d837a44803b0987627
ryanchaput <99510232+ryanchaput@users.noreply.github.com>
2024-04-16 20:16:56 -0400

    Assignment 6 exercises complete

--- App.js
+++ App.js
@@ -1,15 +1,40 @@
+import "dotenv/config";
 import express from 'express';
+import mongoose from 'mongoose';
+import UserRoutes from './Users/routes.js';
 import Hello from './Hello.js';
 import Lab5 from './Lab5.js';
 import cors from 'cors';
 import CourseRoutes from './Kanbas/courses/routes.js';
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import session from "express-session";
 
+const CONNECTION_STRING = process.env.DB_CONECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas")
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+}));
 app.use(express.json());
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUnitinialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+}
+app.use(session(sessionOptions));
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
 Hello(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
